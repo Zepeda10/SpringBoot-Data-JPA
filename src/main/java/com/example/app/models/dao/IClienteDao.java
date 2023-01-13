7006e7900df84a586c9d7294d08a6c0bbee11398
Zepeda10 <hilda_zeped@outlook.com>
2023-01-13 15:56:29 -0600

    Agregando config MySQL - fin de seccion 8

--- src/main/java/com/example/app/models/dao/IClienteDao.java
+++ src/main/java/com/example/app/models/dao/IClienteDao.java
@@ -1,12 +1,9 @@
 package com.example.app.models.dao;
 
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 import com.example.app.models.entity.Cliente;
 
-public interface IClienteDao {
-	public List<Cliente> findAll();
-	public void save(Cliente cliente);
-	public Cliente findOne(Long id);
-	public void delete(Long id);
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
+	
 }

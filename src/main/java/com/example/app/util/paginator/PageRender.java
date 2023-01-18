fec9b5eeeb3ec79e216d35b84774bf86e5065d1c
Zepeda10 <hilda_zeped@outlook.com>
2023-01-18 15:11:58 -0600

    Fin de seccion 9, paginador agregado

--- src/main/java/com/example/app/util/paginator/PageRender.java
+++ src/main/java/com/example/app/util/paginator/PageRender.java
@@ -10,7 +10,7 @@ public class PageRender<T>{
 	private Page<T> page;
 	private int totalPaginas;
 	private int numElementosPorPagina;
-	private int paginaActcual;
+	private int paginaActual;
 	private List<PageItem> paginas;
 	
 	public PageRender(String url, Page<T> page) {
@@ -20,7 +20,7 @@ public class PageRender<T>{
 		
 		numElementosPorPagina = page.getSize();
 		totalPaginas = page.getTotalPages();
-		paginaActcual = page.getNumber() + 1;
+		paginaActual = page.getNumber() + 1;
 		
 		int desde, hasta;
 		
@@ -28,20 +28,20 @@ public class PageRender<T>{
 			desde = 1;
 			hasta = totalPaginas;
 		}else {
-			if(paginaActcual <= numElementosPorPagina/2) {
+			if(paginaActual <= numElementosPorPagina/2) {
 				desde = 1;
 				hasta = numElementosPorPagina;
-			}else if(paginaActcual >= totalPaginas - numElementosPorPagina/2) {
+			}else if(paginaActual >= totalPaginas - numElementosPorPagina/2) {
 				desde = totalPaginas - numElementosPorPagina + 1;
 				hasta = numElementosPorPagina;
 			}else {
-				desde = paginaActcual - numElementosPorPagina/2;
+				desde = paginaActual - numElementosPorPagina/2;
 				hasta = numElementosPorPagina;
 			}
 		}
 		
 		for(int i = 0; i < hasta; i++) {
-			paginas.add(new PageItem(desde + i, paginaActcual == desde + i));
+			paginas.add(new PageItem(desde + i, paginaActual == desde + i));
 		}
 		
 	}
@@ -54,8 +54,8 @@ public class PageRender<T>{
 		return totalPaginas;
 	}
 
-	public int getPaginaActcual() {
-		return paginaActcual;
+	public int getPaginaActual() {
+		return paginaActual;
 	}
 
 	public List<PageItem> getPaginas() {

e80f7bb54e126890756fdd63d44be26a0f07969f
Zepeda10 <hilda_zeped@outlook.com>
2023-01-24 16:39:57 -0600

    Agregando clase ItemFactura

--- src/main/java/com/example/app/models/entity/Factura.java
+++ src/main/java/com/example/app/models/entity/Factura.java
@@ -1,37 +1,58 @@
 package com.example.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
 @Entity
-@Table(name="facturas")
+@Table(name = "facturas")
 public class Factura implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String descripcion;
 	private String observacion;
-	
+
 	@Temporal(TemporalType.DATE)
-	@Column(name="create_at")
+	@Column(name = "create_at")
 	private Date createAt;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id") // Llave foránea en la tabla facturas_items, esto debido a que la relación es en
+										// un solo sentido
+	private List<ItemFactura> items;
+	
+	public Factura() {
+		this.items = new ArrayList<ItemFactura>();
+	}
+
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
 
 	public Long getId() {
 		return id;
@@ -71,6 +92,10 @@ public class Factura implements Serializable {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	
+	public void addItemFactura(ItemFactura item) {
+		this.items.add(item);
 	}
 
 }

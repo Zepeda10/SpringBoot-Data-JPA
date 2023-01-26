539a2e2c4a0c2d896df0ae34ad318ae257b8974f
Zepeda10 <hilda_zeped@outlook.com>
2023-01-26 16:28:29 -0600

    Agreganfo funcion guardar en FacturaController

--- src/main/java/com/example/app/controllers/FacturaController.java
+++ src/main/java/com/example/app/controllers/FacturaController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -21,31 +23,38 @@ import com.example.app.models.service.IClienteService;
 @RequestMapping("/factura")
 @SessionAttributes("factura")
 public class FacturaController {
-	
+
 	@Autowired
 	private IClienteService clienteService;
 
 	@GetMapping("/form/{clienteId}")
-	public String crear(@PathVariable(value="clienteId") Long clienteId, Map<String, Object> model, RedirectAttributes flash) {
-		
-		Cliente cliente = clienteService.findOne(clienteId); //Encontrar cliente de la factura
-		if(cliente == null) {
+	public String crear(@PathVariable(value = "clienteId") Long clienteId, Map<String, Object> model,
+			RedirectAttributes flash) {
+
+		Cliente cliente = clienteService.findOne(clienteId); // Encontrar cliente de la factura
+		if (cliente == null) {
 			flash.addFlashAttribute("error", "El cliente no existe en la base de datos.");
 			return "redirect:/listar";
 		}
-		
+
 		Factura factura = new Factura();
-		factura.setCliente(cliente); //Asignando cliente a factura
-		
+		factura.setCliente(cliente); // Asignando cliente a factura
+
 		model.put("factura", factura);
 		model.put("titulo", "Crear Factura");
-		
+
 		return "factura/form";
 	}
-	
-	@GetMapping(value="/cargar-productos/{term}", produces={"application/json"})
-	public @ResponseBody List<Producto> cargarProducto(@PathVariable String term){
+
+	@GetMapping(value = "/cargar-productos/{term}", produces = { "application/json" })
+	public @ResponseBody List<Producto> cargarProducto(@PathVariable String term) {
 		return clienteService.findByNombre(term);
 	}
-	
+
+	@PostMapping("/form")
+	public String guardar(Factura factura, @RequestParam(name = "item_id[]", required = false) Long[] itemId,
+			@RequestParam(name = "cantidad[]", required = false) Integer[] cantidad, RedirectAttributes flash) {
+		return "";
+	}
+
 }

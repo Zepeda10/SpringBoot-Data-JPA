7f945e4b6088325d06856d4810b8c4ff35548453
Zepeda10 <hilda_zeped@outlook.com>
2023-02-03 12:30:08 -0600

    Agregando nombre de usuario en el mensaje de inicio de sesion

--- src/main/java/com/example/app/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/example/app/auth/handler/LoginSuccessHandler.java
@@ -22,7 +22,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
 		SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
 		FlashMap flashMap = new FlashMap();
 		
-		flashMap.put("success", "Ha iniciado sesión con éxito");
+		flashMap.put("success", "Hola "+authentication.getName()+", has iniciado sesión con éxito");
 		flashMapManager.saveOutputFlashMap(flashMap, request, response);
 		
 		super.onAuthenticationSuccess(request, response, authentication);

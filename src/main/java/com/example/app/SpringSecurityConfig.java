1dcaabc8d4ebc31f514685cab76d0217816e9948
Zepeda10 <hilda_zeped@outlook.com>
2023-02-03 12:27:23 -0600

    Agregando mensaje de inicio se sesion

--- src/main/java/com/example/app/SpringSecurityConfig.java
+++ src/main/java/com/example/app/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,8 +10,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.app.auth.handler.LoginSuccessHandler;
+
 @Configuration
 public class SpringSecurityConfig {
+	
+	@Autowired
+	private LoginSuccessHandler successHandler;
 
 	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
@@ -29,7 +35,9 @@ public class SpringSecurityConfig {
                 .requestMatchers("/eliminar/**").hasAnyRole("ADMIN")
                 .requestMatchers("/factura/**").hasAnyRole("ADMIN")
                 .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login")
+                .and().formLogin()
+                .successHandler(successHandler)
+                .loginPage("/login")
                 .permitAll()
                 .and().logout()
                 .permitAll()

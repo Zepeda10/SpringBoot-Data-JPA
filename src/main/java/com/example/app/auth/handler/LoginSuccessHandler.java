f3abb95c2e7b7d091ef1e581ac31c5dc47a2a1e7
Zepeda10 <hilda_zeped@outlook.com>
2023-02-07 12:18:49 -0600

    Obteniendo usuario autenticado en el controlador

--- src/main/java/com/example/app/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/example/app/auth/handler/LoginSuccessHandler.java
@@ -22,7 +22,12 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
 		SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
 		FlashMap flashMap = new FlashMap();
 		
-		flashMap.put("success", "Hola "+authentication.getName()+", has iniciado sesión con éxito");
+		flashMap.put("success", "Hola " + authentication.getName() + ", has iniciado sesión con éxito");
+		
+		if(authentication != null) {
+			logger.info("El usuario '"+ authentication.getName() +"' ha iniciado sesión con éxito.");
+		}
+		
 		flashMapManager.saveOutputFlashMap(flashMap, request, response);
 		
 		super.onAuthenticationSuccess(request, response, authentication);

c7f35a3a8b41219e4adba56ce64ad151a6a31071
Zepeda10 <hilda_zeped@outlook.com>
2023-02-16 09:26:54 -0600

    Creando repositorio JPA IUsuarioDao

---
+++ src/main/java/com/example/app/models/dao/IUsuarioDao.java
@@ -0,0 +1,9 @@
+package com.example.app.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.app.models.entity.Usuario;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long>{
+	public Usuario findByUsername(String username);
+}
